47846844717ae295e79dc4a482b5bd73ccdef6d4
Aradhya <128085597+Aradhya-Joshi@users.noreply.github.com>
2023-06-08 01:01:59 +0530

    Initial Commit.

---
+++ lib/screens/result_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/components/bottom_button.dart';
+
+class ResultPage extends StatefulWidget {
+  const ResultPage({super.key, required this.resultText, required this.interpretation, required this.bmiResult,});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+              'BMI Calculator',
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(15.0),
+                alignment: Alignment.bottomLeft,
+                child: const Text(
+                  'Your Result',
+                  style: kTitleTextStyle,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: ReusableCard(
+              colour: kActiveCardColour,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.resultText.toUpperCase(),
+                      style: kResultTextStyle,
+                    ),
+                    Text(
+                      widget.bmiResult,
+                      style: kBMITextStyle,
+                    ),
+                    Text(
+                      widget.interpretation,
+                      textAlign: TextAlign.center,
+                      style: kBodyTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            BottomButton(
+              buttonTitle: 'RE-CALCULATE',
+              onTap: (){
+                Navigator.pop(context);
+              }
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
